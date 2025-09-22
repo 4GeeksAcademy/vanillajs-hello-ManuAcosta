fcd771c3ccacca896ed551656d9c8441b8308b5e
Manuel Omar Acosta Dominguez <macosta.ia88@gmail.com>
2025-09-22 08:03:55 +0000

    Generador Dominios Manuel Acosta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generadorDeDominios();
 };
+
+function generadorDeDominios() {
+  // 1. Arrays
+  let pronombre = ['yo', 'tu', 'el', 'nosotros', 'ustedes', 'ellos'];
+  let adjetivo = ['brillante', 'feo', 'bonito', 'alegre', 'enojado', 'oscuro'];
+  // Palabras para hacks
+  let sustantivo = ['perro', 'codigo', 'coche', 'juego', 'edificio', 'planeta', 'internet'];
+  let extension = ['.com', '.es', '.io', '.net', '.org', '.info', '.mx', '.dev'];
+
+  // 2. Bucles
+  console.log("Iniciando la generación de dominios...");
+
+  for (let i = 0; i < pronombre.length; i++) {
+    for (let j = 0; j < adjetivo.length; j++) {
+      for (let k = 0; k < sustantivo.length; k++) {
+        // 3. Agregamos un cuarto bucle para las extensiones
+        for (let l = 0; l < extension.length; l++) {
+
+          // URL Dominio
+          let dominioUno = pronombre[i] + adjetivo[j] + sustantivo[k] + extension[l];
+          console.log(dominioUno);
+
+          // Domain Hack
+          let sinPunto = extension[l].slice(1);
+
+          // Comprobamos si el final del sustantivo es igual a la extensión
+          if (sustantivo[k].endsWith(sinPunto)) {
+            // Si coincide, creamos el "hack"
+            let hack = pronombre[i] + adjetivo[j] + sustantivo[k].slice(0, -sinPunto.length) + extension[l];
+            console.log('Domain Hack ' + hack);
+          }
+        }
+      }
+    }
+  }
+
+  console.log("¡Generación de dominios completada!");
+}
